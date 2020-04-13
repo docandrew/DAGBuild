539ba799f1a99c6d2d5fa170e56d3d411fd54238
docandrew <jon@formallyverified.net>
2020-04-12 23:34:53 -0500

    Added button widget

--- src/dagbuild-gui.adb
+++ src/dagbuild-gui.adb
@@ -1,27 +1,45 @@
 
-with SDL.Events.Keyboards;
+with Interfaces.C; use Interfaces.C;
+
 with SDL.Events.Events;
+with SDL.Events.Keyboards;
+with SDL.Events.Mice;
 
 with SDL.Video.Palettes;
-with SDL.Video.Renderers.Makers;
 with SDL.Video.Renderers;
+with SDL.Video.Renderers.Makers;
+
+with DAGBuild.GUI.State;
+with DAGBuild.GUI.Widgets;
 
 with DAGBuild.Settings;
 
 package body DAGBuild.GUI is
-   
-    -- Draw a SDL Rectangle
-    procedure Draw_Rect(r : in out SDL.Video.Renderers.Renderer;
-                        x : SDL.Coordinate;
-                        y : SDL.Coordinate;
-                        w : SDL.Positive_Dimension;
-                        h : SDL.Positive_Dimension;
-                        c : SDL.Video.Palettes.Colour)
+
+    -- Start a round of widget drawing/interaction
+    procedure IMGUI_Start(st : in out DAGBuild.GUI.State.UIState)
     is
     begin
-        r.Set_Draw_Colour (c);
-        r.Fill (Rectangle => (x, y, w, h));
-    end Draw_Rect;
+        st.Hot_Item := DAGBuild.GUI.State.ID(0);
+    end IMGUI_Start;
+
+    -- Finish a round
+    -- If mouse isn't down, clear the active item.
+    -- If mouse is clicked with no widget active, mark no active item (-1) so we
+    -- don't "click" on something if we release the button on top of another
+    -- widget.
+    procedure IMGUI_Finish(st : in out DAGBuild.GUI.State.UIState)
+    is
+        use DAGBuild.GUI.State;
+    begin
+        if st.Mouse_Down = False then
+            st.Active_Item := NO_ITEM;
+        else
+            if st.Active_Item = NO_ITEM then
+                st.Active_Item := INVALID_ITEM;
+            end if;
+        end if;
+    end IMGUI_Finish;
 
     -- Clear Screen
     procedure Clear_Window(r : in out SDL.Video.Renderers.Renderer;
@@ -33,30 +51,79 @@ package body DAGBuild.GUI is
     end Clear_Window;
 
     -- Render screen elements
-    procedure Render(r : in out SDL.Video.Renderers.Renderer)
+    procedure Render(st : in out DAGBuild.GUI.State.UIState)
     is
+        Click : Boolean;
     begin
-        Clear_Window(r, DAGBuild.Settings.Default_Dark_BG);
-        Draw_Rect(r, 64, 48, 64, 48, c => (16#70#, 16#7A#, 16#8C#, 16#FF#));
-        r.Present;
-        delay 0.100;
+        Clear_Window(st.Renderer, DAGBuild.Settings.Dark_BG);
+
+        IMGUI_Start(st);
+
+        Click := DAGBuild.GUI.Widgets.Button(1,
+                                             st,
+                                             50,
+                                             50);
+
+        Click := DAGBuild.GUI.Widgets.Button(2,
+                                             st,
+                                             150,
+                                             50);
+
+        Click := DAGBuild.GUI.Widgets.Button(3,
+                                             st,
+                                             50,
+                                             150);
+
+        if Click then
+            DAGBuild.Settings.Dark_BG := (0, 255, 255, 255);
+        end if;
+
+        Click := DAGBuild.GUI.Widgets.Button(4,
+                                             st,
+                                             150,
+                                             150);
+        if Click then
+            st.Done := True;
+        end if;
+        
+        IMGUI_Finish(st);
+
+        st.Renderer.Present;
+        delay 0.02; -- TODO: make a "delay until"
     end Render;
 
-    function Handle_Inputs(Event : in out SDL.Events.Events.Events) return Boolean
+    procedure Handle_Inputs(st : in out DAGBuild.GUI.State.UIState)
     is
-        Done : Boolean := False;
+        use type SDL.Events.Mice.Buttons;
+
+        Event       : SDL.Events.Events.Events;
     begin
         while SDL.Events.Events.Poll(Event) loop
 
             case Event.Common.Event_Type is
+
+                when SDL.Events.Mice.Motion =>
+                    st.mouse_x := Event.Mouse_Motion.X;
+                    st.mouse_y := Event.Mouse_Motion.Y;
+                
+                when SDL.Events.Mice.Button_Down =>
+                    if Event.Mouse_Button.Button = SDL.Events.Mice.Left then
+                        st.Mouse_Down := True;
+                    end if;
+
+                when SDL.Events.Mice.Button_Up =>
+                    if Event.Mouse_Button.Button = SDL.Events.Mice.Left then
+                        st.Mouse_Down := False;
+                    end if;
+
                 when SDL.Events.Quit =>
-                    Done := True;
+                    st.Done := True;
             
-                when SDL.Events.Keyboards.Key_Down =>
+                when SDL.Events.Keyboards.Key_Up =>
                     
                     case Event.Keyboard.Key_Sym.Key_Code is
                         when SDL.Events.Keyboards.Code_Escape =>
-                            Done := True;
+                            st.Done := True;
                         when others =>
                             null;
                     end case;
@@ -67,25 +134,20 @@ package body DAGBuild.GUI is
 
         end loop;
         
-        return Done;
-
     end Handle_Inputs;
 
     procedure Event_Loop(Window : in out SDL.Video.Windows.Window)
     is
-        Done : Boolean := False;
-        r : SDL.Video.Renderers.Renderer;
-        Event : SDL.Events.Events.Events;
+        GUI_State   : DAGBuild.GUI.State.UIState;       
     begin
         -- Create hardware renderer if available
-        SDL.Video.Renderers.Makers.Create(r, Window);
+        SDL.Video.Renderers.Makers.Create(GUI_State.Renderer, Window);
 
         loop
-            Render(r);
+            Render(GUI_State);
+            Handle_Inputs(GUI_State);
 
-            Done := Handle_Inputs(Event);
-
-            exit when Done;
+            exit when GUI_State.Done;
         end loop;
 
     end Event_Loop;
--- src/dagbuild-settings.ads
+++ src/dagbuild-settings.ads
@@ -18,9 +18,24 @@ package DAGBuild.Settings is
     );
 
     -- Color defaults, overrideable in user prefs
-    Default_Dark_BG     : constant SDL.Video.Palettes.Colour := (Red    => 16#23#, 
-                                                                 Green  => 16#28#,
-                                                                 Blue   => 16#34#,
-                                                                 Alpha  => 16#FF#);
+    Dark_BG         : SDL.Video.Palettes.Colour := (Red    => 16#23#,
+                                                    Green  => 16#28#,
+                                                    Blue   => 16#34#,
+                                                    Alpha  => 16#FF#);
+
+    Dark_Widget     : SDL.Video.Palettes.Colour := (Red    => 16#70#,
+                                                    Green  => 16#7A#,
+                                                    Blue   => 16#8C#,
+                                                    Alpha  => 16#FF#);
+
+    Dark_Active     : SDL.Video.Palettes.Colour := (Red    => 16#CB#,
+                                                    Green  => 16#CC#,
+                                                    Blue   => 16#C6#,
+                                                    Alpha  => 16#FF#);
+
+    Dark_Text       : SDL.Video.Palettes.Colour := (Red    => 16#CB#,
+                                                    Green  => 16#CC#,
+                                                    Blue   => 16#C6#,
+                                                    Alpha  => 16#FF#);
 
 end DAGBuild.Settings;
